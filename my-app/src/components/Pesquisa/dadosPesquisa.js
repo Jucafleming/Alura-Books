1b0743868ae025bd570b63f6501f99666823a1cd
Joao Fleming <j.fleming@dobesone.com.br>
2024-07-15 14:03:29 -0300

    pesquisa e dadosPesquisa

--- my-app/src/components/Pesquisa/dadosPesquisa.js
+++ my-app/src/components/Pesquisa/dadosPesquisa.js
@@ -2,9 +2,9 @@ import livro from '../../images/livro.png'
 
 
 export const livros = [
-    {nome: 'O guia definitivo do blablabla', id: 1, scr: livro  },
-    {nome: 'O guia  quase definitivo do blablabla', id: 2, scr: livro  },
-    {nome: 'O guia semi-definitivo do blablabla', id: 3, scr: livro  },
-    {nome: 'O guia definitivo final do blablabla', id: 4, scr: livro  },
+    {nome: 'O guia definitivo do blablabla', id: 1, src: livro  },
+    {nome: 'O guia  quase definitivo do blablabla', id: 2, src: livro  },
+    {nome: 'O guia semi-definitivo do blablabla', id: 3, src: livro  },
+    {nome: 'O guia definitivo final do blablabla', id: 4, src: livro  },
 ]
 
