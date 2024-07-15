e63bb0d1be29e877ad37dcc4cafd4dfe31a1d090
Joao Fleming <j.fleming@dobesone.com.br>
2024-07-15 15:00:29 -0300

    cardRecomenda

--- my-app/src/components/UltimosLancamentos/ultimosLancamentos.js
+++ my-app/src/components/UltimosLancamentos/ultimosLancamentos.js
@@ -1,6 +1,8 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
 import { Titulo } from '../Titulo/titulo'
+import CardRecomenda from '../cardRecomenda/cardRecomenda'
+import imagemLivro from '../../images/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -29,6 +31,12 @@ function UltimosLancamentos() {
                     <img src={livro.src} />
                 ))}
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
