c3c9e037e6e404c206085f4b48e0147d8b2fb2fa
Joao Fleming <j.fleming@dobesone.com.br>
2024-07-19 09:36:52 -0300

    criando services e json pra armazenamento

--- backend/routes/livro.js
+++ backend/routes/livro.js
@@ -1,9 +1,22 @@
 const { Router } = require("express")
+const { getLivros } = require('../controller/livros')
 
 const router = Router();
 
-router.get('/', (req,res) => {
-    res.send("Hello World!")
+router.get('/', getLivros);
+
+router.post('/', (req, res) => {
+    res.send("Post!")
 });
+
+router.patch('/', (req, res) => {
+    res.send("patch!")
+});
+
+router.delete('/', (req, res) => {
+    res.send("delete!")
+});
+
+
 
 module.exports = router;
---
+++ backend/services/livro.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+
+function getTodosLivros(){
+    const livros = JSON.parse( fs.readFileSync("livros.json"));
+    return livros;
+}
+
+
+module.exports =  {
+    getTodosLivros
+}
